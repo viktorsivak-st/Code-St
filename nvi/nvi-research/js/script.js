d23e642760514ed3f4da2a3989af67ca07017738
viktorsivak-st <d3@s-t.studio>
2022-06-28 14:53:14 +0300

    update

--- nvi/nvi-research/js/script.js
+++ nvi/nvi-research/js/script.js
@@ -1,17 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 $('.count').each(function () {
    $(this).prop('Counter',0).animate({
     Counter: $(this).text()
     }, {
-     duration: 2000,
+     duration: 5000,
      easing: 'swing',
      step: function (now) {
         $(this).text(Math.ceil(now));
      }
     });
 });
-}); 
+});
+
+
+
 
 
 
