dd1abba8b828b44b54d5c1e0b5e1cbfd5cd0957b
viktorsivak-st <d3@s-t.studio>
2022-06-29 18:45:48 +0300

    Last Updates

---
+++ nvi/nvi-research/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+$('.count').each(function () {
+   $(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+    }, {
+     duration: 5000,
+     easing: 'swing',
+     step: function (now) {
+        $(this).text(Math.ceil(now));
+     }
+    });
+});
+});
+
+
+
+
+
+
+let big = document.querySelector('#big');
+let medium = document.querySelector('#medium');
+let smallLeft = document.querySelector('#small-left');
+let smallRight = document.querySelector('#small-right');
+let smallTop = document.querySelector('#small-top');
+
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    big.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+    medium.style.transform = 'translate(-' + x * 40 + 'px, -' + y * 40 + 'px)';
+    smallLeft.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+    smallRight.style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
+    smallTop.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
+});
+
+
+
+
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+const menuHidden = document.querySelector(".menu-hidden");
+const menuBg = document.querySelector(".menu-bg");
+
+menuOpen.onclick = function () {
+  menuHidden.style.display = 'block';
+  return false;
+}
+
+menuClose.onclick = function () {
+  menuHidden.style.display = 'none';
+  return false;
+}
+
+menuBg.onclick = function () {
+  menuHidden.style.display = 'none';
+  return false;
+}
+
+
+
+
---
+++ nvi/nvi-research/owl.GIS/js/script.js
@@ -0,0 +1,98 @@
+
+
+
+
+(function(){
+
+let counter = document.querySelectorAll('.num');
+let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+window.addEventListener('scroll', function(){  
+  if( limit == counter.length ){ return; }
+  
+  for(let i = 0; i < counter.length; i++){
+    let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+    let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+    if( pos < win && counter[i].dataset.stop === "0" ){
+      counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+      let x = 0;
+      limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+      let int = setInterval(function(){
+        // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+        x = x + Math.ceil( counter[i].dataset.to / 50 ); 
+        counter[i].innerText = x;
+        if( x > counter[i].dataset.to ){
+          //Как только досчитали - стираем интервал.
+          counter[i].innerText = counter[i].dataset.to;
+          clearInterval(int);
+        }
+      }, 60);
+    }
+  }
+});
+
+})();
+
+
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+const menuHidden = document.querySelector(".menu-hidden");
+const menuBg = document.querySelector(".menu-bg");
+
+menuOpen.onclick = function () {
+  menuHidden.style.display = 'block';
+  return false;
+}
+
+menuClose.onclick = function () {
+  menuHidden.style.display = 'none';
+  return false;
+}
+
+menuBg.onclick = function () {
+  menuHidden.style.display = 'none';
+  return false;
+}
+
+
+
+const docOpen = document.querySelector("#doc-btn");
+const docClose = document.querySelector("#doc-close");
+const docHidden = document.querySelector(".doc-container");
+const bgDoc = document.querySelector(".doc-wrapper");
+const nviCon = document.querySelector(".nvi-research");
+
+
+docOpen.onclick = function () {
+  docHidden.style.display = 'block';
+  nviCon.style.position = "fixed";
+  return false;
+}
+
+docClose.onclick = function () {
+  docHidden.style.display = 'none';
+  nviCon.style.position = "relative";
+  return false;
+}
+
+bgDoc.onclick = function () {
+  docHidden.style.display = 'none';
+  nviCon.style.position = "relative";
+  return false;
+}
