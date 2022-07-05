0333ca678947d829d579e898293158496adba95d
viktorsivak-st <d3@s-t.studio>
2022-07-05 14:10:31 +0300

    touchSensitivity

--- nvi/nvi-research-site/js/slider.js
+++ nvi/nvi-research-site/js/slider.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
         menu: '#myMenu',
         direction: 'vertical',
         scrollingSpeed: 1000,
-        touchSensitivity: 10,
+        touchSensitivity: 1000,
         easing: 'swing',
         css3: true,
         navigation: false,
